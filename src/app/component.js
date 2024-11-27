85efb3e7f7d9751fbd73dc1ccca72401f6ecb217
Khosbayar707 <torutora0415@gmail.com>
2024-11-27 17:51:22 +0800

    at class 11 27

---
+++ src/app/component.js
@@ -0,0 +1,36 @@
+const Component = ({ props }) => {
+  return (
+    <div key={props.id} className="card">
+      <h2>
+        {props.firstName} {props.lastName}
+      </h2>
+      <p>{props.companyName}</p>
+      <p>
+        <img
+          src="/picture/mail.png"
+          alt="Mail Icon"
+          style={{ width: "8px", height: "8px", marginRight: "8px" }}
+        />
+        {props.email}
+      </p>
+      <p>
+        <img
+          src="/picture/call.png"
+          alt="Mail call"
+          style={{ width: "8px", height: "8px", marginRight: "8px" }}
+        />
+        {props.phoneNumber}
+      </p>
+      <p>
+        <img
+          src="/picture/location.png"
+          alt="Location"
+          style={{ width: "8px", height: "8px", marginRight: "8px" }}
+        />
+        {props.address}
+      </p>
+    </div>
+  );
+};
+
+export { Component };
